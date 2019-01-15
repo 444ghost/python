f5db90808d5bc39b6617a57b4e5a9c816a5956e1
444ghost <444ghost.git@gmail.com>
2019-01-15 13:46:06 +0900

    2DArray

--- 2DArray.py
+++ 2DArray.py
@@ -12,6 +12,7 @@ def hourglassSum(arr):
             local = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
             if(local > maxHourGlassSum):
                 maxHourGlassSum = local # hey
+
     return maxHourGlassSum
 
 if __name__ == '__main__':
