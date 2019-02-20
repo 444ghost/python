d9c0e41252a41348d136a810b390c57f6649ac60
444ghost <444ghost.git@gmail.com>
2019-02-20 14:54:17 +0900

    Product

--- Product.py
+++ Product.py
@@ -2,7 +2,7 @@ import itertools
 
 mList, n = input().strip().split(" ")
 
-productList = list(itertools.product(sorted(mList), n))
+productList = list(itertools.product(sorted(mList), repeat=int(n)))
 
 for i in productList:
     print(''.join(i))
