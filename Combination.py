f7e0effa0ffdd0f8c27394a65d4868d4257d572c
444ghost <444ghost.git@gmail.com>
2019-02-20 15:13:05 +0900

    Combination

--- Combination.py
+++ Combination.py
@@ -2,7 +2,9 @@ import itertools
 
 mList, n = input().strip().split(" ")
 
-combinationList = list(itertools.combinations(sorted(mList), int(n)))
-
-for i in combinationList:
-    print(''.join(i))
+# combinationList = list(itertools.combinations(sorted(mList), int(n)))
+# print(combinationList)
+for i in range(int(n)):
+    mTuple = list(itertools.combinations(sorted(mList), i + 1))
+    for j in mTuple:
+        print("".join(j))
