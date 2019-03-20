34a715053c514710c9d326f3d1a10aead096d8ec
444ghost <444ghost.git@gmail.com>
2019-03-20 16:18:58 +0900

    Discrete Wavelet Transform Denoising

---
+++ DWT.py
@@ -0,0 +1,72 @@
+import pywt
+import matplotlib.pyplot as plt
+from operator import add
+import numpy as np
+import statistics
+
+# S[I] = (X[2 * I] + X[2 * I + 1]) / sqrt(2)
+# D[I] = (X[2 * I] - X[2 * I + 1]) / sqrt(2)
+
+signal = []
+
+file = open("signal.txt", "r")
+line = file.readline()
+
+while line:
+    signal.append(int(round(float((line[:len(line) - 2])))))
+    line = file.readline()
+file.close()
+
+# print(signal)
+
+noise = np.random.normal(0, 3, len(signal))
+noisySignal = list(map(add, signal, noise))
+
+cA, cD = pywt.dwt(noisySignal, 'haar')
+cA2, cD2 = pywt.dwt(cA, 'haar')
+# print(cA)
+# print(cD)
+
+w = pywt.Wavelet('haar')
+# print(w)
+# print(w.filter_bank)
+
+plt.figure(1)
+plt.subplot(421)
+plt.plot(signal)
+plt.title("original")
+plt.subplot(422)
+plt.plot(noisySignal)
+plt.title("noisy")
+plt.subplot(423)
+plt.plot(cD)
+plt.title("1 level detail")
+plt.subplot(424)
+plt.plot(cD2)
+plt.title("2 level detail")
+plt.subplot(425)
+plt.plot(cA2)
+plt.title("2 level approximation")
+
+std_cD = statistics.stdev(cD)
+std_cD2 = statistics.stdev(cD2)
+
+denoised_cD = pywt.threshold(cD, std_cD, mode='garrote')
+denoised_cD2 = pywt.threshold(cD2, std_cD2, mode='garrote')
+
+icA = pywt.idwt(cA2, denoised_cD2, 'haar')
+denoised_signal = pywt.idwt(icA, denoised_cD, 'haar')
+
+plt.subplot(426)
+plt.plot(denoised_cD)
+plt.title("1 level detail denoised")
+plt.subplot(427)
+plt.plot(denoised_cD2)
+plt.title("2 level detail denoised")
+plt.subplot(428)
+plt.plot(denoised_signal)
+plt.title("denoised signal")
+
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+plt.show()
